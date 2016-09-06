94a7844cc6461e19aa958d057cb63cfd16d048ec
Asrar <asrar@asrar-mac.local>
2016-09-06 21:20:32 +0100

    Step 2 check in

--- src/main/java/com/hmrc/service/ShoppingBasket.java
+++ src/main/java/com/hmrc/service/ShoppingBasket.java
@@ -2,37 +2,67 @@ package com.hmrc.service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.Currency;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class ShoppingBasket {
 
-	ProductRepository productrepository;
+	private final ProductRepository productrepository;
 	
-	public ShoppingBasket(ProductRepository productrepository)
+	Map<String,Function<Integer,Integer>> offers;
+	
+	public ShoppingBasket(ProductRepository productrepository, Map<String,Function<Integer,Integer>> offers)
 	{
 		this.productrepository = productrepository;
+		this.offers= offers;
+		
 	}
 	
 	
 	public BigDecimal getTotal(List<String> items) {
 		
+		Map<String,Long> groupedItems =  items.stream().collect(
+                Collectors.groupingBy(
+                        Function.identity(), Collectors.counting()
+                )
+        );
 
-		Integer total = 0;
-		for(String  item: items){
-			
-			total += productrepository.getItemPrice(item);
+		return getTotal(groupedItems);
+
+	}
+	
+	private BigDecimal getTotal(Map<String,Long> items){
+		BigDecimal discount = getDiscount(items);
+		BigDecimal total = new BigDecimal("0.00");
+		for(Entry<String,Long> entry : items.entrySet()){
+			double subtotal = productrepository.getItemPrice(entry.getKey())*entry.getValue();
+			total = total.add( new BigDecimal(subtotal));
 		}
+		return total.subtract(discount).divide(new BigDecimal("100")).setScale(2,RoundingMode.CEILING);
+		
+	}
 
-		return new BigDecimal(total).divide(new BigDecimal(100)).setScale(2,RoundingMode.CEILING);
+
+	private BigDecimal getDiscount(Map<String, Long> items) {
+		BigDecimal discount = new BigDecimal("0.00");
+		
+		for(Entry<String,Long> entry : items.entrySet()){
+			
+		
+			Integer itemdiscount = 	offers.getOrDefault(entry.getKey(), (quantity) ->{return 0;})
+					.apply(entry.getValue().intValue());//its safe to assume on this occasion that quantity is not over Int.max
+			discount = discount.add( new BigDecimal(itemdiscount));
+		}
+		return discount;
 	}
 	
 	
-
+	
 	
 
 }
--- src/test/java/com/hmrc/service/ShoppingBasketTest.java
+++ src/test/java/com/hmrc/service/ShoppingBasketTest.java
@@ -7,6 +7,7 @@ import java.math.RoundingMode;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,45 +15,115 @@ import org.junit.Test;
 public class ShoppingBasketTest {
 
 	ProductRepository repository;
-	
+
+	Map<String, Function<Integer, Integer>> offers = new HashMap();
+
 	ShoppingBasket basket;
+
 	@Before
 	public void setUp() throws Exception {
-		
-		Map<String,Integer> products = new HashMap<String,Integer>();
-		
+
+		Map<String, Integer> products = new HashMap<String, Integer>();
+
 		products.put("apple", 60);
 		products.put("orange", 25);
 		repository = new ProductRepository(products);
 
-		basket = new ShoppingBasket(repository);
-		
-	}	
+		basket = new ShoppingBasket(repository, offers);
+
+	}
 
 	@Test
 	public void testGetTotal() {
-	
-		BigDecimal total = basket.getTotal(Arrays.asList("apple","apple","orange","apple"));
-		
-		assertEquals(new BigDecimal(2.05).setScale(2,RoundingMode.CEILING),total);
-	}
-	
-	
-	@Test
-	public void testGetTotalCaseSensitive(){
 
-			BigDecimal total = basket.getTotal(Arrays.asList("APPLE","apple","Orange","apple"));
-			
-			assertEquals(new BigDecimal(2.05).setScale(2,RoundingMode.CEILING),total);
+		BigDecimal total = basket.getTotal(Arrays.asList("apple", "apple",
+				"orange", "apple"));
+
+		assertEquals(new BigDecimal("2.05").setScale(2, RoundingMode.CEILING),
+				total);
 	}
-	
+
+	@Test
+	public void testGetTotalCaseSensitive() {
+
+		BigDecimal total = basket.getTotal(Arrays.asList("APPLE", "apple",
+				"Orange", "apple"));
+
+		assertEquals(new BigDecimal("2.05").setScale(2, RoundingMode.CEILING),
+				total);
+	}
+
 	@Test
 	public void testGetTotalNoItems() {
-	
+
 		BigDecimal total = basket.getTotal(Arrays.asList());
+
+		assertEquals(new BigDecimal("0.00").setScale(2, RoundingMode.CEILING), total);
+	}
+
+	@Test
+	public void testGetTotalAppleOffer() {
+		addOffers();
+		basket = new ShoppingBasket(repository, offers);
+
+		BigDecimal total = basket.getTotal(Arrays.asList("apple", "apple"));
+
+		assertEquals(new BigDecimal("0.60").setScale(2, RoundingMode.CEILING),
+				total);
 		
-		assertEquals(new BigDecimal(0).setScale(2,RoundingMode.CEILING),total);
 	}
 	
+	@Test
+	public void testGetTotalOrangeOffer() {
+		addOffers();
+		basket = new ShoppingBasket(repository, offers);
+
+		BigDecimal total = basket.getTotal(Arrays.asList("orange", "orange", "orange"));
+
+		assertEquals(new BigDecimal("0.50").setScale(2, RoundingMode.CEILING),
+				total);
+		
+	}
+	
+	@Test
+	public void testGetTotalCombinationOffer() {
+		addOffers();
+		basket = new ShoppingBasket(repository, offers);
+
+		BigDecimal total = basket.getTotal(Arrays.asList("apple","apple","orange", "orange", "orange"));
+
+		assertEquals(new BigDecimal("1.10").setScale(2, RoundingMode.CEILING),
+				total);
+		
+	}
+	
+	@Test
+	public void testGetTotalCombinationOfferDoubleItems() {
+		addOffers();
+		basket = new ShoppingBasket(repository, offers);
+
+		BigDecimal total = basket.getTotal(Arrays.asList("apple","apple","apple","apple","orange",
+				"orange", "orange","orange", "orange", "orange"));
+
+		assertEquals(new BigDecimal("2.20").setScale(2, RoundingMode.CEILING),
+				total);
+		
+	}
+
+
+
+	private void addOffers() {
+		offers.put("apple", (quantity) -> {
+			Integer price = repository.getItemPrice("apple");
+			Integer discount = new Double(quantity / 2).intValue() * price;
+			return discount;
+		});
+		
+		offers.put("orange", (quantity) -> {
+			Integer price = repository.getItemPrice("orange");
+			Integer discount = new Double(quantity / 3).intValue() * price;
+			return discount;
+		});
+	}
 
 }
